77a9fd9362dc30a47d1929aa9460ea87aa6e2748
Tracey Bullington <traceylbullington@gmail.com>
2023-06-13 10:49:49 -0500

    Initial Commit

---
+++ scrollView/scrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrollViewApp.swift
+//  scrollView
+//
+//  Created by scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct scrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
