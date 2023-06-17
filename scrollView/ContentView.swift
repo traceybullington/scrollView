8999da25337fb8551334a3457ca68c914fa7b88c
Tracey Bullington <traceylbullington@gmail.com>
2023-06-17 11:52:16 -0500

    Simple scroll view example (you can scroll through multiple images or elements vertically)

--- scrollView/ContentView.swift
+++ scrollView/ContentView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView (showsIndicators: true) {
+            VStack(spacing: 20) {
+                ForEach(0..<2) {
+                    Text("Item \($0)")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .frame(width: 400, height: 1200)
+                        .background(.red)
+                }
+            }
         }
-        .padding()
+        .frame(height: 350)
+        
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
